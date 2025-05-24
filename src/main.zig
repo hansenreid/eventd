b495ac97bd6bb11a5c93b153806fa4853371f65f
Reid Hansen <rhansen@influxdata.com>
2025-05-24 08:26:03 -0500

    chore: Move network code to new file

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const builtin = @import("builtin");
 const linux_io = @import("io/linux.zig");
 const wasm_io = @import("io/wasm.zig");
 const pg_wire = @import("pg-wire.zig");
+const network = @import("network.zig");
 
 const native_endian = @import("builtin").target.cpu.arch.endian();
 
@@ -36,8 +37,8 @@ export fn run() void {
     io.log("Hello There");
 
     const bytes = [_]u8{ 0x12, 0x32, 0x32, 0x12 };
-    const non_empty = pg_wire.NonEmptyBytes.init(&bytes);
-    var deserializer = pg_wire.Deserializer.init(non_empty);
+    const non_empty = network.NonEmptyBytes.init(&bytes);
+    var deserializer = network.Deserializer.init(non_empty);
     std.debug.print("{x}\n", .{deserializer.next_int(u16).int});
     std.debug.print("{x}\n", .{deserializer.next_int(u8).int});
     std.debug.print("{x}\n", .{deserializer.next_int(u8).int});
---
+++ src/network.zig
@@ -0,0 +1,123 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const assert = std.debug.assert;
+
+pub const Network = @This();
+
+pub const NonEmptyBytes = struct {
+    items: []const u8,
+
+    comptime {
+        assert(@sizeOf(NonEmptyBytes) == @sizeOf([]const u8));
+    }
+
+    pub fn init(bytes: []const u8) NonEmptyBytes {
+        assert(bytes.len >= 1);
+
+        return .{
+            .items = bytes,
+        };
+    }
+
+    pub inline fn assert_invariants(self: NonEmptyBytes) void {
+        assert(self.items.len >= 1);
+    }
+};
+
+pub const Deserializer = struct {
+    bytes: NonEmptyBytes,
+    pos: u32 = 0,
+
+    pub fn init(bytes: NonEmptyBytes) Deserializer {
+        bytes.assert_invariants();
+
+        return .{
+            .bytes = bytes,
+        };
+    }
+
+    pub fn next_int(self: *Deserializer, T: type) NativeInt(T) {
+        const size = @sizeOf(T);
+        assert(self.pos + size <= self.bytes.items.len);
+        const bytes = self.bytes.items[self.pos .. self.pos + size];
+
+        const int = NetworkInt(T).init(std.mem.readVarInt(T, bytes, .big));
+
+        self.pos += size;
+        return NativeInt(T).from_network(int);
+    }
+};
+
+pub fn NetworkInt(comptime T: type) type {
+    if (@typeInfo(T) != .int) {
+        @compileError("only ints accepted");
+    }
+
+    return struct {
+        int: T,
+        const this = @This();
+
+        comptime {
+            assert(@sizeOf(this) == @sizeOf(T));
+        }
+
+        pub fn bytes(self: this) T {
+            return self.int;
+        }
+
+        pub fn init(int: T) this {
+            return .{
+                .int = int,
+            };
+        }
+
+        pub inline fn from_native(native: NativeInt(T)) this {
+            return init(std.mem.nativeToBig(T, native.int));
+        }
+    };
+}
+
+pub fn NativeInt(comptime T: type) type {
+    if (@typeInfo(T) != .int) {
+        @compileError("only ints accepted");
+    }
+
+    return struct {
+        int: T,
+        const this = @This();
+
+        comptime {
+            assert(@sizeOf(this) == @sizeOf(T));
+        }
+
+        pub fn bytes(self: this) T {
+            return self.int;
+        }
+
+        pub fn init(int: T) this {
+            return .{
+                .int = int,
+            };
+        }
+
+        pub inline fn from_network(network: NetworkInt(T)) this {
+            return init(std.mem.bigToNative(T, network.int));
+        }
+    };
+}
+
+test "can transform native int to network int and back" {
+    const native = NativeInt(u16).init(0x1234);
+    const network = NetworkInt(u16).from_native(native);
+
+    const result = NativeInt(u16).from_network(network);
+    try expect(std.meta.eql(native, result));
+}
+
+test "can transform netowrk int to native int and back" {
+    const network = NetworkInt(u16).init(0x1234);
+    const native = NativeInt(u16).from_network(network);
+
+    const result = NetworkInt(u16).from_native(native);
+    try expect(std.meta.eql(network, result));
+}
