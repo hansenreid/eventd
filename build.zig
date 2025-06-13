d313e87a9919187caa7f7d309661a24ac08a61b2
Reid Hansen <=>
2025-06-13 17:11:56 -0500

    chore: Cleanup Build file

--- build.zig
+++ build.zig
@@ -17,7 +17,14 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const benchmark_mod = b.createModule(.{
+        .root_source_file = b.path("src/benchmark.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     exe_mod.addOptions("build_options", options);
+    benchmark_mod.addOptions("build_options", options);
 
     exe_mod.export_symbol_names = &.{
         "run",
@@ -28,10 +35,17 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
 
+    const benchmark_exe = b.addExecutable(.{
+        .name = "eventd_benchmark",
+        .root_module = benchmark_mod,
+    });
+
     b.installArtifact(exe);
+    const benchmark_install = b.addInstallArtifact(benchmark_exe, .{});
+    const benchmark_step = b.step("benchmark", "Build the benchmark");
+    benchmark_step.dependOn(&benchmark_install.step);
 
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
@@ -50,46 +64,21 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 
-    const benchmark_mod = b.createModule(.{
-        .root_source_file = b.path("src/benchmark.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    benchmark_mod.addOptions("build_options", options);
-
-    benchmark_mod.export_symbol_names = &.{
-        "run",
-    };
-
-    const benchmark_exe = b.addExecutable(.{
-        .name = "eventd_benchmark",
-        .root_module = benchmark_mod,
-    });
-
-    const benchmark_install = b.addInstallArtifact(benchmark_exe, .{});
-    const benchmark_run = b.addRunArtifact(benchmark_exe);
-
-    const benchmark_step = b.step("benchmark", "Build the benchmark");
-    benchmark_step.dependOn(&benchmark_install.step);
-
-    const benchmark_run_step = b.step("run-bench", "Build the benchmark");
-    benchmark_run_step.dependOn(&benchmark_run.step);
-
     if (tracy_enable) {
-        const client_cpp = "../../tools/tracy/public/TracyClient.cpp";
-        const tracy_c_flags: []const []const u8 = if (target.result.os.tag == .windows and target.result.abi == .gnu)
-            &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined", "-D_WIN32_WINNT=0x601" }
-        else
-            &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined" };
-
-        exe.addIncludePath(.{ .cwd_relative = "../../tools/tracy" });
-        benchmark_exe.addIncludePath(.{ .cwd_relative = "../../tools/tracy" });
-        exe.addCSourceFile(.{ .file = .{ .cwd_relative = client_cpp }, .flags = tracy_c_flags });
-        benchmark_exe.addCSourceFile(.{ .file = .{ .cwd_relative = client_cpp }, .flags = tracy_c_flags });
-        exe.root_module.linkSystemLibrary("c++", .{ .use_pkg_config = .no });
-        benchmark_exe.root_module.linkSystemLibrary("c++", .{ .use_pkg_config = .no });
-        exe.linkLibC();
-        benchmark_exe.linkLibC();
+        setup_tracy(exe, target);
+        setup_tracy(benchmark_exe, target);
     }
+}
+
+fn setup_tracy(c: *std.Build.Step.Compile, t: std.Build.ResolvedTarget) void {
+    const client_cpp = "../../tools/tracy/public/TracyClient.cpp";
+    const tracy_c_flags: []const []const u8 = if (t.result.os.tag == .windows and t.result.abi == .gnu)
+        &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined", "-D_WIN32_WINNT=0x601" }
+    else
+        &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined" };
+
+    c.addIncludePath(.{ .cwd_relative = "../../tools/tracy" });
+    c.addCSourceFile(.{ .file = .{ .cwd_relative = client_cpp }, .flags = tracy_c_flags });
+    c.root_module.linkSystemLibrary("c++", .{ .use_pkg_config = .no });
+    c.linkLibC();
 }
