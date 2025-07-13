ffcf0c7f0855c573400a6769a809f0715b84c8b2
Reid Hansen <=>
2025-07-13 17:58:12 -0500

    feat: Accept a connection

--- src/main.zig
+++ src/main.zig
@@ -4,14 +4,36 @@ const pg_wire = @import("pg-wire.zig");
 const network = @import("network.zig");
 const IOLoop = @import("io_loop.zig");
 
+const net = std.net;
+const posix = std.posix;
+
 const io_impl = @import("io.zig");
 const IO = io_impl.IO;
 const Command = io_impl.Command;
 
 const tracy = @import("tracy.zig");
 
+var done = false;
 pub fn main() !void {
-    run();
+    var io = try IO.init();
+
+    const address = try std.net.Address.parseIp("127.0.0.1", 5678);
+    const fd = try io.open_socket(address.any.family);
+    defer posix.close(fd);
+
+    const resolved_address = try io.listen(fd, address);
+    _ = resolved_address;
+
+    var loop = IOLoop.init(&io);
+    var continuations: [10]IOLoop.Continuation = undefined;
+
+    loop.accept(fd, address.any, address.getOsSockLen(), &continuations[0], dummy);
+
+    while (!done) {
+        loop.tick();
+        std.debug.print("Ticking\n", .{});
+        std.Thread.sleep(std.time.ns_per_s);
+    }
 }
 
 fn dummy(context: *anyopaque, continuation: *IOLoop.Continuation) void {
@@ -19,22 +41,7 @@ fn dummy(context: *anyopaque, continuation: *IOLoop.Continuation) void {
     _ = loop;
 
     std.debug.print("result: {any}\n", .{continuation.status});
-    std.debug.print("result: {any}\n", .{continuation.command.write.result});
+    std.debug.print("result: {any}\n", .{continuation.command.accept.result});
 
-    std.debug.print("Hello from a dummy function\n", .{});
-}
-
-export fn run() void {
-    var buffer: [256]u8 = undefined;
-
-    var io = IO.init() catch {
-        unreachable;
-    };
-
-    var loop = IOLoop.init(&io);
-    var c: IOLoop.Continuation = undefined;
-    loop.write(3, &buffer, 0, &c, dummy);
-
-    loop.tick();
-    loop.tick();
+    done = true;
 }
